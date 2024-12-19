3fc313e23f4883fcf0dedf21281c3ac27ad83151
Sayrikey <kamalseriki49@gmail.com>
2024-12-19 15:04:03 +0100

    2nd commit

---
+++ test.py
@@ -0,0 +1,3 @@
+ALEMBIC ISSH:https://adex.ltd/database-migrations-with-alembic-and-fastapi-a-comprehensive-guide-using-poetry
+AIVEN: https://aiven.io/docs/products/postgresql/get-started
+uvicorn main:app --reload
